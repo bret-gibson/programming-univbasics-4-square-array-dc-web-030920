4e754b2804e845d532c7c8b4f465d6a87a7f5180
Bret Gibson <bret.gibson@gmail.com>
2020-02-17 18:47:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0;
   newArray = [];
-  while(counter < array.length) do
+  while counter < array.length do
     newVal = array[counter] * array[counter]
     newArray.push(newVal)
     counter++
